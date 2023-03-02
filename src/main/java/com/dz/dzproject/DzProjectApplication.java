9f6b2ff30b9ba18250ae2c43d8ead26b05127b9c
夜宵ww <46333086+elexsggg@users.noreply.github.com>
2023-03-02 16:15:35 +0800

    Add files via upload

---
+++ src/main/java/com/dz/dzproject/DzProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dz.dzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dz/dzproject/DzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dz.dzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
